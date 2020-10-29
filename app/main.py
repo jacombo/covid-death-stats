03dea2cdb33ff1b12eab50c3a3b98edc39805ea8
jof <dilinges@gmail.com>
2020-10-29 21:52:18 +0100

    omalovanky

--- app/main.py
+++ app/main.py
@@ -1,63 +1,65 @@
-from flask import Flask 
+from flask import Flask
 
 import requests
 from pprint import pprint
 import operator
+from flask import render_template
 
-app = Flask(__name__) 
+app = Flask(__name__,
+            static_url_path='/static')
 
-@app.route("/") 
-def home_view(): 
-		return get_data()
+overview_url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json"
+death_url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/umrti.json"
+github = "https://github.com/jacombo/covid-death-stats"
 
-def get_data():
-    death_url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/umrti.json"
-    github = "https://github.com/jacombo/covid-death-stats"
+@app.route("/")
+def home_view():
+    return render_template('index.html', deaths=deaths_by_age(), general=general_stats())
+
+
+def deaths_by_age():
     response = requests.get(death_url)
-    
-    output = "<h1>Covid 19 pocet umrti od zacatku roku 2020</h1>"
-    output = output + "<h3>Data aktualizovany:%s</h3><hr>" % (response.json()['modified'])
-
+    deaths = dict()
     summary = {}
     age_count = 0
     total_deaths = 0
     for item in response.json()['data']:
-        #print("%s %s" % (item['vek'], round(item['vek'],-1)))
-        age = round(item['vek'],-1)
+        age = round(item['vek'], -1)
         age_count = age_count + age
 
         if age in summary:
             count = summary[age]
         else:
-            count = 1    
-        
+            count = 1
+
         summary[age] = count + 1
         total_deaths = total_deaths + 1
 
     # Sort Dictionary by value using itemgetter
     sorted_dict = dict(sorted(summary.items(),
-                                key=operator.itemgetter(1),
-                                reverse=True))
+                              key=operator.itemgetter(1),
+                              reverse=True))
+    deaths['groups'] = list()
+    for k, v in sorted_dict.items():
+        deaths['groups'].append({ "age": k, "count": v, "percentage": round((v / total_deaths) * 100, 1) })
+    deaths['last_updated'] = response.json()['modified']
+    deaths['average_death_age'] = round(age_count / total_deaths, 1)
+    deaths['total_deaths'] = total_deaths
+    return deaths
 
-    for k,v in sorted_dict.items():
-        output = output + "Vek:%s+&nbsp;&nbsp;&nbsp;&nbsp;pocet umrti:%s&nbsp;&nbsp;&nbsp;&nbsp;%s%%</br>" % (k,v, round((v / total_deaths) * 100, 1))
-
-    output = output + "</br>Prumerny vek mrtvych:%s</br>" % (round(age_count / total_deaths, 1))
-    output = output + "</br>Celkovy pocet mrtvych:%s</br>" % (total_deaths)
-    
-    overview_url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json"
-    response = requests.get(overview_url)        
-
+def general_stats():
+    response = requests.get(overview_url)
     item = response.json()['data'][0]
-    output = output + "<hr><h3>Zakladni prehled ke dni %s</h3>" % (item['datum'])  
-
-    output = output + "Provedene testy celkem:%s</br>" % (item['provedene_testy_celkem'])   
-    output = output + "Potvrzene pripady celkem:%s</br>" % (item['potvrzene_pripady_celkem'])   
-    output = output + "Aktivni pripady:%s</br>" % (item['aktivni_pripady'])   
-    output = output + "Vyleceni:%s</br>" % (item['vyleceni'])   
-    output = output + "Umrti:%s</br>" % (item['umrti'])   
-    output = output + "Aktualne hospitalizovani:%s</br>" % (item['aktualne_hospitalizovani'])   
-    output = output + "Smrtnost: %s / %s * 100 = %s%%</br>" % (item['umrti'], item['potvrzene_pripady_celkem'] , round((item['umrti'] / item['potvrzene_pripady_celkem'] * 100),4))   
-
-    output = output + "</br><hr><a target='_blank' href='%s'>Zdroj dat umrti</a></br><a target='_blank' href='%s'>Zdroj dat zakladni prehled</a></br><a target='_blank' href='%s'>GitHub zdrojaky</a>" % (death_url,overview_url,github)
-    return output
+    general = dict()
+    general['to_date'] = item['datum']
+    general['tests_total'] = item['provedene_testy_celkem']
+    general['confirmed_cases_total'] = item['potvrzene_pripady_celkem']
+    general['active_cases'] = item['aktivni_pripady']
+    general['cured'] = item['vyleceni']
+    general['deaths'] = item['umrti']
+    general['currently_hospitalized'] = item['aktualne_hospitalizovani']
+    general['mortality'] = round((item['umrti'] / item['potvrzene_pripady_celkem'] * 100), 4)
+    general['src_general'] = overview_url
+    general['src_deaths'] = death_url
+    general['src_code'] = github
+    return general
